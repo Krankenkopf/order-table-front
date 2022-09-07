b1c6ad034bd728cd3f2503f3cff04f44be6ecb20
Krankenkopf <petricowork@gmail.com>
2022-09-07 18:32:56 +0300

    api: add apikey header

--- src/api/gql.ts
+++ src/api/gql.ts
@@ -2,11 +2,13 @@ import { createClient, fetchExchange } from "urql";
 console.log(process.env.API_URL);
 
 const client = createClient({
-  url: process.env.API_URL || "http://localhost:3000/graphql",
+  url: process.env.REACT_APP_API_URL || "http://localhost:5000/graphql",
   fetchOptions: () => {
     // Token?
     return {
-      headers: {},
+      headers: {
+        "API-KEY": process.env.REACT_APP_API_KEY!,
+      },
     };
   },
   exchanges: [fetchExchange],
